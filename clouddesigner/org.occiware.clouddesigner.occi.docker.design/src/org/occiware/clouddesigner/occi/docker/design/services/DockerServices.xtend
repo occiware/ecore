ab963a9562aaa6c4c9deee252ef7635cd73768ba
wpiers <william.piers@obeo.fr>
2015-05-07 17:23:03 +0200

    added popup dialogs for modeler actions changed styles for container & machine

--- clouddesigner/org.occiware.clouddesigner.occi.docker.design/src/org/occiware/clouddesigner/occi/docker/design/services/DockerServices.xtend
+++ clouddesigner/org.occiware.clouddesigner.occi.docker.design/src/org/occiware/clouddesigner/occi/docker/design/services/DockerServices.xtend
@@ -4,13 +4,19 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *	- Fawaz PARAISO 
+ * - Fawaz PARAISO 
  *******************************************************************************/
 package org.occiware.clouddesigner.occi.docker.design.services
 
+import java.lang.reflect.InvocationTargetException
+import org.eclipse.core.runtime.IProgressMonitor
 import org.eclipse.emf.ecore.EObject
+import org.eclipse.jface.dialogs.ProgressMonitorDialog
+import org.eclipse.jface.operation.IRunnableWithProgress
+import org.eclipse.swt.widgets.Display
+import org.eclipse.swt.widgets.Shell
 import org.occiware.clouddesigner.OCCI.Configuration
 import org.occiware.clouddesigner.occi.docker.Container
 import org.occiware.clouddesigner.occi.docker.Machine
@@ -26,61 +32,94 @@ class DockerServices {
 	 * Popup menu Start action.
 	 */
 	def void start(EObject eo) {
-		if (eo instanceof Machine) {
-			var machine = eo as Machine
-			val main = new ExecutableDockerModel(machine)
-			main.start
+		var runnable = new IRunnableWithProgress() {
+			override run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+				if (eo instanceof Machine) {
+					var machine = eo as Machine
+					val main = new ExecutableDockerModel(machine)
+					main.start
 
-		} else if (eo instanceof Container) {
-			var container = eo as Container
-			val main = new ExecutableDockerModel(container)
-			main.container.start
+				} else if (eo instanceof Container) {
+					var container = eo as Container
+					val main = new ExecutableDockerModel(container)
+					main.container.start
+				}
+			}
 		}
+		var dialog = new ProgressMonitorDialog(getShell())
+		dialog.run(false, true, runnable)
 	}
 
 	/**
 	 * Popup menu importModel cation.
 	 */
 	def void importModel(Configuration conf) {
-		val main = new ExecutableDockerModel(conf)
-		main.importModel
+		var runnable = new IRunnableWithProgress() {
+			override run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+				val main = new ExecutableDockerModel(conf)
+				main.importModel
+			}
+		}
+		var dialog = new ProgressMonitorDialog(getShell())
+		dialog.run(false, true, runnable)
 	}
 
 	/**
 	 * Popum menu synchronize action.
 	 */
 	def void synchronize(EObject eo) {
-		if (eo instanceof Machine) {
-			var machine = eo as Machine
-			val main = new ExecutableDockerModel(machine)
-			main.synchronize
+		var runnable = new IRunnableWithProgress() {
+			override run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+				if (eo instanceof Machine) {
+					var machine = eo as Machine
+					val main = new ExecutableDockerModel(machine)
+					main.synchronize
+				}
+			}
 		}
+		var dialog = new ProgressMonitorDialog(getShell())
+		dialog.run(false, true, runnable)
 	}
 
 	/**
 	 * Popup menu StartAll action
 	 */
 	def void startAll(EObject eo) {
-		if (eo instanceof Machine) {
-			var machine = eo as Machine
-			val main = new ExecutableDockerModel(machine)
-			main.startAll
-
+		var runnable = new IRunnableWithProgress() {
+			override run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+				if (eo instanceof Machine) {
+					var machine = eo as Machine
+					val main = new ExecutableDockerModel(machine)
+					main.startAll
+				}
+			}
 		}
+		var dialog = new ProgressMonitorDialog(getShell())
+		dialog.run(false, true, runnable)
 	}
 
 	/**
 	 * Popup menu stop action.
 	 */
 	def void stop(EObject eo) {
-		if (eo instanceof Machine) {
-			var machine = eo as Machine
-			val main = new ExecutableDockerModel(machine)
-			main.stop
-		} else if (eo instanceof Container) {
-			var container = eo as Container
-			val main = new ExecutableDockerModel(container)
-			main.stop
+		var runnable = new IRunnableWithProgress() {
+			override run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+				if (eo instanceof Machine) {
+					var machine = eo as Machine
+					val main = new ExecutableDockerModel(machine)
+					main.stop
+				} else if (eo instanceof Container) {
+					var container = eo as Container
+					val main = new ExecutableDockerModel(container)
+					main.stop
+				}
+			}
 		}
+		var dialog = new ProgressMonitorDialog(getShell())
+		dialog.run(false, true, runnable)
+	}
+	
+	def Shell getShell() {
+		return Display.current.activeShell
 	}
 }
--- clouddesigner/org.occiware.clouddesigner.occi.docker.design/xtend-gen/org/occiware/clouddesigner/occi/docker/design/services/DockerServices.java
+++ clouddesigner/org.occiware.clouddesigner.occi.docker.design/xtend-gen/org/occiware/clouddesigner/occi/docker/design/services/DockerServices.java
@@ -6,11 +6,18 @@
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- * 	- Fawaz PARAISO
+ * - Fawaz PARAISO
  */
 package org.occiware.clouddesigner.occi.docker.design.services;
 
+import java.lang.reflect.InvocationTargetException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.xtext.xbase.lib.Exceptions;
 import org.occiware.clouddesigner.OCCI.Configuration;
 import org.occiware.clouddesigner.occi.docker.Container;
 import org.occiware.clouddesigner.occi.docker.DockerFactory;
@@ -26,16 +33,27 @@ public class DockerServices {
    * Popup menu Start action.
    */
   public void start(final EObject eo) {
-    if ((eo instanceof Machine)) {
-      Machine machine = ((Machine) eo);
-      final ExecutableDockerModel main = new ExecutableDockerModel(machine);
-      main.start();
-    } else {
-      if ((eo instanceof Container)) {
-        Container container = ((Container) eo);
-        final ExecutableDockerModel main_1 = new ExecutableDockerModel(container);
-        main_1.container.start();
-      }
+    try {
+      IRunnableWithProgress runnable = new IRunnableWithProgress() {
+        public void run(final IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+          if ((eo instanceof Machine)) {
+            Machine machine = ((Machine) eo);
+            final ExecutableDockerModel main = new ExecutableDockerModel(machine);
+            main.start();
+          } else {
+            if ((eo instanceof Container)) {
+              Container container = ((Container) eo);
+              final ExecutableDockerModel main_1 = new ExecutableDockerModel(container);
+              main_1.container.start();
+            }
+          }
+        }
+      };
+      Shell _shell = this.getShell();
+      ProgressMonitorDialog dialog = new ProgressMonitorDialog(_shell);
+      dialog.run(false, true, runnable);
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
     }
   }
   
@@ -43,18 +61,40 @@ public class DockerServices {
    * Popup menu importModel cation.
    */
   public void importModel(final Configuration conf) {
-    final ExecutableDockerModel main = new ExecutableDockerModel(conf);
-    main.importModel();
+    try {
+      IRunnableWithProgress runnable = new IRunnableWithProgress() {
+        public void run(final IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+          final ExecutableDockerModel main = new ExecutableDockerModel(conf);
+          main.importModel();
+        }
+      };
+      Shell _shell = this.getShell();
+      ProgressMonitorDialog dialog = new ProgressMonitorDialog(_shell);
+      dialog.run(false, true, runnable);
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
   }
   
   /**
    * Popum menu synchronize action.
    */
   public void synchronize(final EObject eo) {
-    if ((eo instanceof Machine)) {
-      Machine machine = ((Machine) eo);
-      final ExecutableDockerModel main = new ExecutableDockerModel(machine);
-      main.synchronize();
+    try {
+      IRunnableWithProgress runnable = new IRunnableWithProgress() {
+        public void run(final IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+          if ((eo instanceof Machine)) {
+            Machine machine = ((Machine) eo);
+            final ExecutableDockerModel main = new ExecutableDockerModel(machine);
+            main.synchronize();
+          }
+        }
+      };
+      Shell _shell = this.getShell();
+      ProgressMonitorDialog dialog = new ProgressMonitorDialog(_shell);
+      dialog.run(false, true, runnable);
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
     }
   }
   
@@ -62,10 +102,21 @@ public class DockerServices {
    * Popup menu StartAll action
    */
   public void startAll(final EObject eo) {
-    if ((eo instanceof Machine)) {
-      Machine machine = ((Machine) eo);
-      final ExecutableDockerModel main = new ExecutableDockerModel(machine);
-      main.startAll();
+    try {
+      IRunnableWithProgress runnable = new IRunnableWithProgress() {
+        public void run(final IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+          if ((eo instanceof Machine)) {
+            Machine machine = ((Machine) eo);
+            final ExecutableDockerModel main = new ExecutableDockerModel(machine);
+            main.startAll();
+          }
+        }
+      };
+      Shell _shell = this.getShell();
+      ProgressMonitorDialog dialog = new ProgressMonitorDialog(_shell);
+      dialog.run(false, true, runnable);
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
     }
   }
   
@@ -73,16 +124,32 @@ public class DockerServices {
    * Popup menu stop action.
    */
   public void stop(final EObject eo) {
-    if ((eo instanceof Machine)) {
-      Machine machine = ((Machine) eo);
-      final ExecutableDockerModel main = new ExecutableDockerModel(machine);
-      main.stop();
-    } else {
-      if ((eo instanceof Container)) {
-        Container container = ((Container) eo);
-        final ExecutableDockerModel main_1 = new ExecutableDockerModel(container);
-        main_1.stop();
-      }
+    try {
+      IRunnableWithProgress runnable = new IRunnableWithProgress() {
+        public void run(final IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+          if ((eo instanceof Machine)) {
+            Machine machine = ((Machine) eo);
+            final ExecutableDockerModel main = new ExecutableDockerModel(machine);
+            main.stop();
+          } else {
+            if ((eo instanceof Container)) {
+              Container container = ((Container) eo);
+              final ExecutableDockerModel main_1 = new ExecutableDockerModel(container);
+              main_1.stop();
+            }
+          }
+        }
+      };
+      Shell _shell = this.getShell();
+      ProgressMonitorDialog dialog = new ProgressMonitorDialog(_shell);
+      dialog.run(false, true, runnable);
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
     }
+  }
+  
+  public Shell getShell() {
+    Display _current = Display.getCurrent();
+    return _current.getActiveShell();
   }
 }
