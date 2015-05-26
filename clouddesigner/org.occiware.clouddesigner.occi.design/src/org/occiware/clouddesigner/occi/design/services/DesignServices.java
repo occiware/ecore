d854d22c8da203625802623532a1ad377a422c12
wpiers <william.piers@obeo.fr>
2015-05-26 17:10:16 +0200

    added import action on OCCI designer

--- clouddesigner/org.occiware.clouddesigner.occi.design/src/org/occiware/clouddesigner/occi/design/services/DesignServices.java
+++ clouddesigner/org.occiware.clouddesigner.occi.design/src/org/occiware/clouddesigner/occi/design/services/DesignServices.java
@@ -1,10 +1,17 @@
 package org.occiware.clouddesigner.occi.design.services;
 
-import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
-
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.edit.ui.action.LoadResourceAction.LoadResourceDialog;
+import org.eclipse.sirius.business.api.session.Session;
+import org.eclipse.sirius.business.api.session.SessionManager;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.occiware.clouddesigner.OCCI.Action;
 import org.occiware.clouddesigner.OCCI.Attribute;
+import org.occiware.clouddesigner.OCCI.Extension;
 
 public class DesignServices {
 
@@ -13,24 +20,46 @@ public class DesignServices {
 		return eObj.eClass() == EcorePackage.eINSTANCE.getEDataType();
 	}
 
-    /**
-     * Computes the label of an Action.
-     */
-    public String render(Action action) {
-    	System.out.println("Render " + action); //$NON-NLS-1$
+	/**
+	 * Computes the label of an Action.
+	 */
+	public String render(Action action) {
+		System.out.println("Render " + action); //$NON-NLS-1$
 		StringBuilder sb = new StringBuilder();
 		sb.append(action.getTerm());
 		sb.append('(');
 		boolean first = true;
-		for(Attribute param : action.getAttributes()) {
+		for (Attribute param : action.getAttributes()) {
 			if (!first) {
 				sb.append(", "); //$NON-NLS-1$
 			} else {
 				first = false;
 			}
-			sb.append(param.getName()).append(" : ").append(param.getType().getName()); //$NON-NLS-1$
+			sb.append(param.getName())
+					.append(" : ").append(param.getType().getName()); //$NON-NLS-1$
 		}
 		sb.append(')');
 		return sb.toString();
-    }
+	}
+
+	public void importExtension(Extension extension) {
+		Shell shell = Display.getCurrent().getActiveShell();
+		Session session = SessionManager.INSTANCE.getSession(extension);
+		LoadResourceDialog dialog = new LoadResourceDialog(shell,
+				session.getTransactionalEditingDomain());
+		dialog.open();
+
+		for (URI uri : dialog.getURIs()) {
+			Resource resource = session.getTransactionalEditingDomain()
+					.getResourceSet().getResource(uri, true);
+			if (!resource.getContents().isEmpty()
+					&& (resource.getContents().get(0) instanceof Extension)
+					&& !extension.getImport().contains(
+							resource.getContents().get(0))) {
+				extension.getImport().add(
+						(Extension) resource.getContents().get(0));
+			}
+		}
+
+	}
 }
