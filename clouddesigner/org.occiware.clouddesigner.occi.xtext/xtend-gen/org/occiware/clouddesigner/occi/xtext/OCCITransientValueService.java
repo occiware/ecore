041e64865439c4413a743e7910209c356ece9d2b
William Piers <william.piers@obeo.fr>
2015-07-08 15:14:59 +0200

    removed .gitignore files generated by egit: for now xtend/xtext is not generated during build, so we need to commit all java files (even derived ones

---
+++ clouddesigner/org.occiware.clouddesigner.occi.xtext/xtend-gen/org/occiware/clouddesigner/occi/xtext/OCCITransientValueService.java
@@ -0,0 +1,26 @@
+package org.occiware.clouddesigner.occi.xtext;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.xtext.parsetree.reconstr.impl.DefaultTransientValueService;
+import org.occiware.clouddesigner.occi.Category;
+import org.occiware.clouddesigner.occi.OCCIPackage;
+
+@SuppressWarnings("all")
+public class OCCITransientValueService extends DefaultTransientValueService {
+  @Override
+  public boolean isTransient(final EObject owner, final EStructuralFeature feature, final int index) {
+    boolean _and = false;
+    if (!(owner instanceof Category)) {
+      _and = false;
+    } else {
+      int _featureID = feature.getFeatureID();
+      boolean _equals = (OCCIPackage.CATEGORY__SCHEME == _featureID);
+      _and = _equals;
+    }
+    if (_and) {
+      return true;
+    }
+    return super.isTransient(owner, feature, index);
+  }
+}
