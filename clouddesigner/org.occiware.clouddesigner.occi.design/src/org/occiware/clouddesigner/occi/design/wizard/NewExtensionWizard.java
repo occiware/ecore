28512e9cd696e4bfb335ae8d562d79b28d377612
William Piers <william.piers@obeo.fr>
2015-07-21 14:57:32 +0200

    added check to the extension wizard

--- clouddesigner/org.occiware.clouddesigner.occi.design/src/org/occiware/clouddesigner/occi/design/wizard/NewExtensionWizard.java
+++ clouddesigner/org.occiware.clouddesigner.occi.design/src/org/occiware/clouddesigner/occi/design/wizard/NewExtensionWizard.java
@@ -49,19 +49,20 @@ import org.occiware.clouddesigner.occi.OCCIRegistry;
 import org.occiware.clouddesigner.occi.design.Activator;
 import org.occiware.clouddesigner.occi.design.Messages;
 
+import com.google.common.base.Strings;
+
 /**
  * The wizard to create a new Extension designer project.
  *
- * @author Stephane Thibaudeau <a
- *         href="mailto:stephane.thibaudeau@obeo.fr">stephane
+ * @author Stephane Thibaudeau
+ *         <a href="mailto:stephane.thibaudeau@obeo.fr">stephane
  *         .thibaudeau@obeo.fr</a>
- * @author Melanie Bats <a
- *         href="mailto:melanie.bats@obeo.fr">melanie.bats@obeo.fr</a>
+ * @author Melanie Bats
+ *         <a href="mailto:melanie.bats@obeo.fr">melanie.bats@obeo.fr</a>
  */
 public class NewExtensionWizard extends BasicNewProjectResourceWizard {
 
-	private final class NewExtensionWizardPage extends
-			WizardNewProjectCreationPage {
+	private final class NewExtensionWizardPage extends WizardNewProjectCreationPage {
 		private static final String OCCI_CORE_EXTENSION_SCHEME = "http://schemas.ogf.org/occi/core#";
 		private CheckboxTableViewer refExtensionViewer;
 
@@ -78,12 +79,10 @@ public class NewExtensionWizard extends BasicNewProjectResourceWizard {
 			GridLayout layout = new GridLayout();
 			layout.numColumns = 2;
 			extensionGroup.setLayout(layout);
-			extensionGroup
-					.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+			extensionGroup.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 
 			Label projectLabel = new Label(extensionGroup, SWT.NONE);
-			projectLabel
-					.setText(Messages.NewExtensionWizard_ExtensionNameLabel);
+			projectLabel.setText(Messages.NewExtensionWizard_ExtensionNameLabel);
 			projectLabel.setFont(parent.getFont());
 
 			extensionNameText = new Text(extensionGroup, SWT.BORDER);
@@ -96,12 +95,12 @@ public class NewExtensionWizard extends BasicNewProjectResourceWizard {
 				@Override
 				public void modifyText(ModifyEvent e) {
 					extensionName = extensionNameText.getText();
+					setPageComplete(validatePage());
 				}
 			});
 
 			Label schemeLabel = new Label(extensionGroup, SWT.NONE);
-			schemeLabel
-					.setText(Messages.NewExtensionWizard_ExtensionSchemeLabel);
+			schemeLabel.setText(Messages.NewExtensionWizard_ExtensionSchemeLabel);
 			schemeLabel.setFont(parent.getFont());
 
 			extensionSchemeText = new Text(extensionGroup, SWT.BORDER);
@@ -113,14 +112,13 @@ public class NewExtensionWizard extends BasicNewProjectResourceWizard {
 				@Override
 				public void modifyText(ModifyEvent e) {
 					extensionScheme = extensionSchemeText.getText();
+					setPageComplete(validatePage());
 				}
 			});
 
 			Label refOccieLabel = new Label(extensionGroup, SWT.NONE);
-			refOccieLabel.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, true,
-					true));
-			refOccieLabel
-					.setText(Messages.NewExtensionWizard_RefExtensionLabel);
+			refOccieLabel.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, true, true));
+			refOccieLabel.setText(Messages.NewExtensionWizard_RefExtensionLabel);
 			refOccieLabel.setFont(parent.getFont());
 
 			Composite composite = new Composite(extensionGroup, SWT.NULL);
@@ -129,13 +127,11 @@ public class NewExtensionWizard extends BasicNewProjectResourceWizard {
 			TableColumnLayout tableLayout = new TableColumnLayout();
 			composite.setLayout(tableLayout);
 
-			Table table = new Table(composite, SWT.CHECK | SWT.MULTI
-					| SWT.BORDER | SWT.FULL_SELECTION | SWT.H_SCROLL
-					| SWT.V_SCROLL);
+			Table table = new Table(composite,
+					SWT.CHECK | SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION | SWT.H_SCROLL | SWT.V_SCROLL);
 			refExtensionViewer = new CheckboxTableViewer(table);
 
-			refExtensionViewer.setContentProvider(ArrayContentProvider
-					.getInstance());
+			refExtensionViewer.setContentProvider(ArrayContentProvider.getInstance());
 
 			Collection<String> registeredExtensions = new ArrayList<String>(
 					OCCIRegistry.getInstance().getRegisteredExtensions());
@@ -145,8 +141,14 @@ public class NewExtensionWizard extends BasicNewProjectResourceWizard {
 
 		public String[] getRefExtensionSchemes() {
 			return Arrays.copyOf(refExtensionViewer.getCheckedElements(),
-					refExtensionViewer.getCheckedElements().length,
-					String[].class);
+					refExtensionViewer.getCheckedElements().length, String[].class);
+		}
+
+		@Override
+		protected boolean validatePage() {
+			// TODO add error messages
+			return super.validatePage() && !Strings.isNullOrEmpty(extensionNameText.getText().trim())
+					&& !Strings.isNullOrEmpty(extensionSchemeText.getText().trim());
 		}
 	}
 
@@ -173,24 +175,19 @@ public class NewExtensionWizard extends BasicNewProjectResourceWizard {
 
 	private Text extensionSchemeText;
 
-	private void openRepresentation(IProject curProject,
-			IProgressMonitor monitor) {
-		final Option<ModelingProject> optionalModelingProject = ModelingProject
-				.asModelingProject(curProject);
+	private void openRepresentation(IProject curProject, IProgressMonitor monitor) {
+		final Option<ModelingProject> optionalModelingProject = ModelingProject.asModelingProject(curProject);
 		if (optionalModelingProject.some()) {
 			final Session session = optionalModelingProject.get().getSession();
 			if (session != null) {
 				if (!session.getSelectedViews().isEmpty()) {
 					for (final DView view : session.getSelectedViews()) {
 						if (!view.getOwnedRepresentations().isEmpty()) {
-							for (final DRepresentation representation : view
-									.getOwnedRepresentations()) {
+							for (final DRepresentation representation : view.getOwnedRepresentations()) {
 								final RepresentationDescription description = DialectManager.INSTANCE
 										.getDescription(representation);
-								if (EXTENSION_DIAGRAM_NAME.equals(description
-										.getName())) {
-									DialectUIManager.INSTANCE.openEditor(
-											session, representation, monitor);
+								if (EXTENSION_DIAGRAM_NAME.equals(description.getName())) {
+									DialectUIManager.INSTANCE.openEditor(session, representation, monitor);
 									return;
 								}
 							}
@@ -211,15 +208,11 @@ public class NewExtensionWizard extends BasicNewProjectResourceWizard {
 	public boolean performFinish() {
 		IProgressMonitor monitor = new NullProgressMonitor();
 		try {
-			project = ModelingProjectManager.INSTANCE.createNewModelingProject(
-					newProjectPage.getProjectName(),
+			project = ModelingProjectManager.INSTANCE.createNewModelingProject(newProjectPage.getProjectName(),
 					newProjectPage.getLocationPath(), true, monitor);
 		} catch (CoreException e) {
-			Activator
-					.getDefault()
-					.getLog()
-					.log(new Status(IStatus.ERROR, Activator.PLUGIN_ID,
-							Messages.NewExtensionWizard_ProjectCreationError, e));
+			Activator.getDefault().getLog().log(new Status(IStatus.ERROR, Activator.PLUGIN_ID,
+					Messages.NewExtensionWizard_ProjectCreationError, e));
 		}
 
 		if (project == null || extensionName == null || extensionScheme == null) {
@@ -228,78 +221,55 @@ public class NewExtensionWizard extends BasicNewProjectResourceWizard {
 
 		final IRunnableWithProgress op = new WorkspaceModifyOperation(null) {
 			@Override
-			protected void execute(IProgressMonitor monitor)
-					throws CoreException, InterruptedException {
+			protected void execute(IProgressMonitor monitor) throws CoreException, InterruptedException {
 				// Do not call super as we don't want to use the super perform
 				// method to create the project,
 				// in our case the project was created using the modeling
 				// project api, we need to extends the
 				// BasicNewProjectResourceWizard to implement the perspective
 				// switch easily.
-				final InitExtensionModel init = new InitExtensionModel(project,
-						extensionName, extensionScheme,
+				final InitExtensionModel init = new InitExtensionModel(project, extensionName, extensionScheme,
 						newProjectPage.getRefExtensionSchemes());
 				try {
 					getContainer().run(false, true, init);
 				} catch (final InterruptedException e) {
 					// Ignore.
 				} catch (final InvocationTargetException e) {
-					Activator
-							.getDefault()
-							.getLog()
-							.log(new Status(
-									IStatus.ERROR,
-									Activator.PLUGIN_ID,
-									Messages.NewExtensionWizard_ModelCreationError,
-									e));
+					Activator.getDefault().getLog().log(new Status(IStatus.ERROR, Activator.PLUGIN_ID,
+							Messages.NewExtensionWizard_ModelCreationError, e));
 				}
 
 				// Get the newly created file
-				final IResource newModelFile = project.findMember(extensionName
-						+ InitExtensionModel.FILE_EXT);
+				final IResource newModelFile = project.findMember(extensionName + InitExtensionModel.FILE_EXT);
 
 				// Switch to the modeling perspective
 				updatePerspective();
 
 				// Select it in the explorer
-				selectAndReveal(newModelFile, PlatformUI.getWorkbench()
-						.getActiveWorkbenchWindow());
+				selectAndReveal(newModelFile, PlatformUI.getWorkbench().getActiveWorkbenchWindow());
 
 				// Init the representation
-				final Option<ModelingProject> optionalModelingProject = ModelingProject
-						.asModelingProject(project);
+				final Option<ModelingProject> optionalModelingProject = ModelingProject.asModelingProject(project);
 				if (optionalModelingProject.some()) {
-					final Session session = optionalModelingProject.get()
-							.getSession();
+					final Session session = optionalModelingProject.get().getSession();
 
-					final EObject rootObject = session.getSemanticResources()
-							.iterator().next().getContents().get(0);
-					final RepresentationDescription representationDescription = getRepresentationDescription(
-							rootObject, session, EXTENSION_DIAGRAM_NAME);
+					final EObject rootObject = session.getSemanticResources().iterator().next().getContents().get(0);
+					final RepresentationDescription representationDescription = getRepresentationDescription(rootObject,
+							session, EXTENSION_DIAGRAM_NAME);
 
-					RecordingCommand createcommand = new RecordingCommand(
-							session.getTransactionalEditingDomain()) {
+					RecordingCommand createcommand = new RecordingCommand(session.getTransactionalEditingDomain()) {
 
 						@Override
 						protected void doExecute() {
-							DialectManager.INSTANCE.createRepresentation(
-									extensionName, rootObject,
-									representationDescription, session,
-									new NullProgressMonitor());
+							DialectManager.INSTANCE.createRepresentation(extensionName, rootObject,
+									representationDescription, session, new NullProgressMonitor());
 						}
 					};
 					try {
-						session.getTransactionalEditingDomain()
-								.getCommandStack().execute(createcommand);
+						session.getTransactionalEditingDomain().getCommandStack().execute(createcommand);
 					} catch (Exception e) {
-						Activator
-								.getDefault()
-								.getLog()
-								.log(new Status(
-										IStatus.ERROR,
-										Activator.PLUGIN_ID,
-										Messages.NewExtensionWizard_RepresentationCreationError,
-										e));
+						Activator.getDefault().getLog().log(new Status(IStatus.ERROR, Activator.PLUGIN_ID,
+								Messages.NewExtensionWizard_RepresentationCreationError, e));
 					}
 					// Open the diagram
 					openRepresentation(project, monitor);
@@ -311,19 +281,14 @@ public class NewExtensionWizard extends BasicNewProjectResourceWizard {
 			getContainer().run(false, true, op);
 
 			// convert to plugin project
-			getContainer().run(
-					false,
-					true,
-					new ConvertProjectToPluginOperation(
-							new IProject[] { project }, false));
+			getContainer().run(false, true, new ConvertProjectToPluginOperation(new IProject[] { project }, false));
 
 			// convert to OCCIE plugin
 			getContainer().run(false, true, new WorkspaceModifyOperation() {
 
 				@Override
 				protected void execute(IProgressMonitor monitor)
-						throws CoreException, InvocationTargetException,
-						InterruptedException {
+						throws CoreException, InvocationTargetException, InterruptedException {
 					configureOCCIEExtension(monitor);
 				}
 
@@ -331,16 +296,11 @@ public class NewExtensionWizard extends BasicNewProjectResourceWizard {
 
 		} catch (final InvocationTargetException e) {
 			if (e.getTargetException() instanceof CoreException) {
-				ErrorDialog.openError(getContainer().getShell(),
-						Messages.NewExtensionWizard_ModelCreationError, null,
+				ErrorDialog.openError(getContainer().getShell(), Messages.NewExtensionWizard_ModelCreationError, null,
 						((CoreException) e.getTargetException()).getStatus());
 			} else {
-				Activator
-						.getDefault()
-						.getLog()
-						.log(new Status(IStatus.ERROR, Activator.PLUGIN_ID,
-								Messages.NewExtensionWizard_ModelCreationError,
-								e));
+				Activator.getDefault().getLog().log(new Status(IStatus.ERROR, Activator.PLUGIN_ID,
+						Messages.NewExtensionWizard_ModelCreationError, e));
 			}
 		} catch (final InterruptedException e) {
 			return false;
@@ -348,35 +308,23 @@ public class NewExtensionWizard extends BasicNewProjectResourceWizard {
 		return true;
 	}
 
-	private void configureOCCIEExtension(IProgressMonitor monitor)
-			throws CoreException {
+	private void configureOCCIEExtension(IProgressMonitor monitor) throws CoreException {
 		IFile manifest = PDEProject.getManifest(project);
-		String manifestContent = "Manifest-Version: 1.0\n"
-				+ "Bundle-ManifestVersion: 2\n" + "Bundle-Name: "
-				+ project.getName() + "\n" + "Bundle-SymbolicName: "
-				+ project.getName() + ";singleton:=true\n"
-				+ "Bundle-Version: 1.0.0.qualifier\n"
-				+ "Require-Bundle: org.occiware.clouddesigner.occi\n";
-		manifest.setContents(
-				new ByteArrayInputStream(manifestContent.getBytes()), true,
-				false, monitor);
+		String manifestContent = "Manifest-Version: 1.0\n" + "Bundle-ManifestVersion: 2\n" + "Bundle-Name: "
+				+ project.getName() + "\n" + "Bundle-SymbolicName: " + project.getName() + ";singleton:=true\n"
+				+ "Bundle-Version: 1.0.0.qualifier\n" + "Require-Bundle: org.occiware.clouddesigner.occi\n";
+		manifest.setContents(new ByteArrayInputStream(manifestContent.getBytes()), true, false, monitor);
 
 		IFile pluginXML = PDEProject.getPluginXml(project);
-		String pluginContent = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-				+ "<?eclipse version=\"3.0\"?>\n"
-				+ "<plugin>\n"
-				+ "<extension point=\"org.occiware.clouddesigner.occi.occie\">\n"
-				+ "<occie file=\"model/" + extensionName + ".occie\" scheme=\""
-				+ extensionScheme + "\">\n" + "</occie>\n" + "</extension>\n"
-				+ "</plugin>\n";
-		pluginXML.create(new ByteArrayInputStream(pluginContent.getBytes()),
-				true, monitor);
+		String pluginContent = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + "<?eclipse version=\"3.0\"?>\n"
+				+ "<plugin>\n" + "<extension point=\"org.occiware.clouddesigner.occi.occie\">\n"
+				+ "<occie file=\"model/" + extensionName + ".occie\" scheme=\"" + extensionScheme + "\">\n"
+				+ "</occie>\n" + "</extension>\n" + "</plugin>\n";
+		pluginXML.create(new ByteArrayInputStream(pluginContent.getBytes()), true, monitor);
 
 		IFile build = PDEProject.getBuildProperties(project);
-		String buildContent = "bin.includes = META-INF/,\\n"
-				+ "               plugin.xml";
-		build.setContents(new ByteArrayInputStream(buildContent.getBytes()),
-				true, false, monitor);
+		String buildContent = "bin.includes = META-INF/,\\n" + "               plugin.xml";
+		build.setContents(new ByteArrayInputStream(buildContent.getBytes()), true, false, monitor);
 	}
 
 	@Override
@@ -386,12 +334,10 @@ public class NewExtensionWizard extends BasicNewProjectResourceWizard {
 		// page.
 		// super.addPages();
 
-		newProjectPage = new NewExtensionWizardPage(
-				Messages.NewExtensionWizard_PageName);
+		newProjectPage = new NewExtensionWizardPage(Messages.NewExtensionWizard_PageName);
 		newProjectPage.setInitialProjectName(""); //$NON-NLS-1$
 		newProjectPage.setTitle(Messages.NewExtensionWizard_PageTitle);
-		newProjectPage
-				.setDescription(Messages.NewExtensionWizard_PageDescription);
+		newProjectPage.setDescription(Messages.NewExtensionWizard_PageDescription);
 		addPage(newProjectPage);
 	}
 
@@ -406,14 +352,12 @@ public class NewExtensionWizard extends BasicNewProjectResourceWizard {
 	 *            Representation description id
 	 * @return Representation description
 	 */
-	private static RepresentationDescription getRepresentationDescription(
-			EObject eObject, Session session, String representationDescriptionId) {
+	private static RepresentationDescription getRepresentationDescription(EObject eObject, Session session,
+			String representationDescriptionId) {
 		final Collection<RepresentationDescription> representationDescriptions = DialectManager.INSTANCE
-				.getAvailableRepresentationDescriptions(
-						session.getSelectedViewpoints(false), eObject);
+				.getAvailableRepresentationDescriptions(session.getSelectedViewpoints(false), eObject);
 		for (final RepresentationDescription representationDescription : representationDescriptions) {
-			if (representationDescriptionId.equals(representationDescription
-					.getName())) {
+			if (representationDescriptionId.equals(representationDescription.getName())) {
 				return representationDescription;
 			}
 		}
