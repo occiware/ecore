753cf8811879ce93defa9f3bd789b525b5623f96
philippemerle <philippe.merle@inria.fr>
2015-10-19 17:24:28 +0200

    Reorganized the code to avoid a null pointer exception.

--- clouddesigner/org.occiware.clouddesigner.occi.xtext/src/org/occiware/clouddesigner/occi/xtext/scoping/NameProvider.java
+++ clouddesigner/org.occiware.clouddesigner.occi.xtext/src/org/occiware/clouddesigner/occi/xtext/scoping/NameProvider.java
@@ -15,28 +15,28 @@ public class NameProvider extends IQualifiedNameProvider.AbstractImpl {
 	private IQualifiedNameConverter qualifiedNameConverter;
 
 	public QualifiedName getFullyQualifiedName(EObject obj) {
-		if (obj.eIsProxy())
+		if (obj.eIsProxy()) {
 			return null;
-		if(obj instanceof EDataType) {
-			return qualifiedNameConverter.toQualifiedName(((EDataType)obj).getName());
 		}
-		String name = new OCCISwitch<String>() {
 
-			public String caseExtension(
-					org.occiware.clouddesigner.occi.Extension object) {
-				return object.getName();
-			};
+		String name = null;
+		if(obj instanceof EDataType) {
+			name = ((EDataType)obj).getName();
+		} else {
+			name = new OCCISwitch<String>() {
+						public String caseExtension(org.occiware.clouddesigner.occi.Extension object) {
+							return object.getName();
+						};
 
-			public String caseKind(org.occiware.clouddesigner.occi.Kind object) {
-				return object.getTerm();
-			};
+						public String caseKind(org.occiware.clouddesigner.occi.Kind object) {
+							return object.getTerm();
+						};
 
-			public String caseMixin(org.occiware.clouddesigner.occi.Mixin object) {
-				return object.getTerm();
-			};
-		}.doSwitch(obj);
-		if (name == null)
-			return null;
-		return qualifiedNameConverter.toQualifiedName(name);
+						public String caseMixin(org.occiware.clouddesigner.occi.Mixin object) {
+							return object.getTerm();
+						};
+				}.doSwitch(obj);
+		}
+		return name != null ? qualifiedNameConverter.toQualifiedName(name) : null;
 	}
 }
