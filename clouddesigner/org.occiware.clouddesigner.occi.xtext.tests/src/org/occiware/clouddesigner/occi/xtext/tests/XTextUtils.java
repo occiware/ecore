34022ee84a5c08c4c0a06fd275a378bbcf9aecb4
William Piers <william.piers@obeo.fr>
2015-10-21 16:48:21 +0200

    added non reg test

--- clouddesigner/org.occiware.clouddesigner.occi.xtext.tests/src/org/occiware/clouddesigner/occi/xtext/tests/XTextUtils.java
+++ clouddesigner/org.occiware.clouddesigner.occi.xtext.tests/src/org/occiware/clouddesigner/occi/xtext/tests/XTextUtils.java
@@ -1,6 +1,11 @@
 package org.occiware.clouddesigner.occi.xtext.tests;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Collections;
 
 import org.eclipse.emf.common.util.URI;
@@ -8,6 +13,7 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.occiware.clouddesigner.occi.OCCIPackage;
 import org.occiware.clouddesigner.occi.xtext.OCCIStandaloneSetup;
 
@@ -16,42 +22,51 @@ public final class XTextUtils {
 	public static ResourceSet resourceSet = new ResourceSetImpl();
 
 	static {
-		resourceSet.getPackageRegistry().put(OCCIPackage.eNS_URI,
-				OCCIPackage.eINSTANCE);
+		resourceSet.getPackageRegistry().put(OCCIPackage.eNS_URI, OCCIPackage.eINSTANCE);
 		OCCIStandaloneSetup.doSetup();
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("occie", new XMIResourceFactoryImpl());
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("occic", new XMIResourceFactoryImpl());
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("txt",
+				resourceSet.getResourceFactoryRegistry().getFactory(URI.createURI("temp.occi")));
 	}
-	
-	public static void parse(URI uri) throws IOException {
-		System.out.println("Loading " + uri + "...");
-		Resource source = resourceSet.getResource(uri, true);
+
+	public static void parse(URI input, URI output) throws IOException {
+		Resource source = resourceSet.getResource(input, true);
 		// SUPER IMPORTANT for resolving cross-references.
 		EcoreUtil.resolveAll(source);
-		System.out.println(uri + " loaded");
-
-		Resource target = resourceSet.createResource(URI
-				.createURI("data/output/"
-						+ uri.lastSegment().replace(".occi", ".xmi")));
-		
+		Resource target = resourceSet.createResource(output);
 		target.getContents().addAll(source.getContents());
-
-		System.out.println("Saving " + target.getURI() + "...");
 		target.save(Collections.EMPTY_MAP);
-		System.out.println(target.getURI() + " saved");
 	}
 
-	public static void serialize(URI uri) throws IOException {
-		System.out.println("Loading " + uri + "...");
-		Resource source = resourceSet.getResource(uri, true);
-		System.out.println(uri + " loaded");
-
-		Resource target = resourceSet.createResource(URI
-				.createURI("data/output/"
-						+ uri.lastSegment().replace(".xmi", ".occi")));
-
+	public static void serialize(URI input, URI output) throws IOException {
+		Resource source = resourceSet.getResource(input, true);
+		Resource target = resourceSet.createResource(output);
 		target.getContents().addAll(source.getContents());
-
-		System.out.println("Saving " + target.getURI() + "...");
 		target.save(Collections.EMPTY_MAP);
-		System.out.println(target.getURI() + " saved");
+	}
+
+	public static String getFileAsString(File file) throws IOException {
+		String result = "";
+		FileInputStream fis = null;
+		BufferedInputStream bis = null;
+		BufferedReader dis = null;
+		StringBuffer sb = new StringBuffer();
+
+		fis = new FileInputStream(file);
+		bis = new BufferedInputStream(fis);
+		dis = new BufferedReader(new InputStreamReader(bis));
+
+		while (dis.ready()) {
+			sb.append(dis.readLine() + "\n");
+		}
+
+		fis.close();
+		bis.close();
+		dis.close();
+
+		result = sb.toString();
+		return result;
+
 	}
 }
