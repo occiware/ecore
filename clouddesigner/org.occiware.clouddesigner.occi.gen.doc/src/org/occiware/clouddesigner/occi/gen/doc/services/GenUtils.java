e796e49d36258986051affcb3429b293973f6e56
philippemerle <philippe.merle@inria.fr>
2015-10-30 21:46:28 +0100

    Improved the Textile documentation generator: * added the date of the generation and the source extension file.

---
+++ clouddesigner/org.occiware.clouddesigner.occi.gen.doc/src/org/occiware/clouddesigner/occi/gen/doc/services/GenUtils.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Inria
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Philippe Merle <philippe.merle@inria.fr>
+ *******************************************************************************/
+package org.occiware.clouddesigner.occi.gen.doc.services;
+
+import java.util.Date;
+import org.occiware.clouddesigner.occi.Extension;
+
+public class GenUtils
+{
+	/**
+	 * Get the current date.
+	 * @return the current date.
+	 */
+	public String getNow() {
+		return new Date().toString();
+	}
+
+	/**
+	 * Get the file path of an extension.
+	 * @param extension the given extension.
+	 * @return the file path of the given extension.
+	 */
+	public String getFilePath(Extension extension) {
+		return extension.eResource().getURI().toString();
+	}
+}
