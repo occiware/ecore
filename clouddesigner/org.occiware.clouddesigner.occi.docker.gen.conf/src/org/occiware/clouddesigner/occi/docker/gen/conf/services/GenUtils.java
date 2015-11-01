6b228f6058105c2016583acfe4de07773fa5eda6
philippemerle <philippe.merle@inria.fr>
2015-11-01 15:25:54 +0100

    Improved generation of deployment scripts with Docker commands: - generated into src-gen/docker/ folder. - generated date and source configuration file path. - cosmetic updates in generation of docker-machine command arguments. - dealt with -p and -v arguments of the docker command.

--- clouddesigner/org.occiware.clouddesigner.occi.docker.gen.conf/src/org/occiware/clouddesigner/occi/docker/gen/conf/services/GenUtils.java
+++ clouddesigner/org.occiware.clouddesigner.occi.docker.gen.conf/src/org/occiware/clouddesigner/occi/docker/gen/conf/services/GenUtils.java
@@ -1,14 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Inria
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Philippe Merle <philippe.merle@inria.fr>
+ *******************************************************************************/
 package org.occiware.clouddesigner.occi.docker.gen.conf.services;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import org.occiware.clouddesigner.occi.Configuration;
 import org.occiware.clouddesigner.occi.Link;
 import org.occiware.clouddesigner.occi.Resource;
 import org.occiware.clouddesigner.occi.docker.Container;
 import org.occiware.clouddesigner.occi.docker.Machine;
 
-public class GenUtils {
+public class GenUtils
+{
+	/**
+	 * Get the current date.
+	 * @return the current date.
+	 */
+	public String getNow() {
+		return new Date().toString();
+	}
+
+	/**
+	 * Get the file path of a configuration.
+	 * @param extension the given configuration.
+	 * @return the file path of the given configuration.
+	 */
+	public String getFilePath(Configuration configuration) {
+		return configuration.eResource().getURI().toString();
+	}
+
+	/**
+	 * Get the filename of a given configuration.
+	 * @param configuration a given configuration.
+	 * @return the filename of the given configuration.
+	 */
+	public String configurationFileName(Configuration configuration) {
+		return configuration.eResource().getURI().lastSegment();
+	}
 
 	public List<Container> getOrderedContainers(Machine machine) {
 		List<Container> res = new ArrayList<Container>();
@@ -26,7 +64,7 @@ public class GenUtils {
 		return res;
 	}
 
-	public Integer toInteger(Float f ) {
+	public Integer toInteger(Float f) {
 		return Math.round(f);
 	}
 }
