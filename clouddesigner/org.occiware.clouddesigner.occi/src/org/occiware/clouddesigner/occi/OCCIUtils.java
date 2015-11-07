c9ba5014ca7690daf9089f906b0d2fe345f9be74
philippemerle <philippe.merle@inria.fr>
2015-11-07 14:11:43 +0100

    Added some utility functions. i.e., convertEclassName2EntityTerm(), getKindByTerm(), getConfiguration().

--- clouddesigner/org.occiware.clouddesigner.occi/src/org/occiware/clouddesigner/occi/OCCIUtils.java
+++ clouddesigner/org.occiware.clouddesigner.occi/src/org/occiware/clouddesigner/occi/OCCIUtils.java
@@ -22,6 +22,25 @@ public final class OCCIUtils
 	private OCCIUtils() {}
 
 	/**
+	 * Convert an OCCI attribute name to an EMF feature name.
+	 * @param attributeName the given OCCI attribute name.
+	 * @return the converted EMF attribute name.
+	 */
+	public static String computeEmfAttributeNameFromOcciAttributeName(final String attributeName) {
+		int index = attributeName.lastIndexOf('.');
+		return (index != -1) ? attributeName.substring(index+1) : attributeName;
+	}
+
+	 /**
+	  * Convert an EClass name to an OCCI entity term.
+	  * @param eclassName the given Eclass's name.
+	  * @return an OCCI entity term.
+	  */
+	 public static String convertEclassName2EntityTerm(String eclassName) {
+		return eclassName.substring(0, 1).toLowerCase() + eclassName.substring(1);
+	 }
+
+	/**
 	 * Get all the attributes of an Entity instance.
 	 * @param entity the given Entity instance.
 	 * @return all the attributes of the given instance.
@@ -29,7 +48,10 @@ public final class OCCIUtils
 	public static Collection<Attribute> getAllAttributes(final Entity entity)
 	{
 		List<Attribute> attributes = new ArrayList<Attribute>();
-		OCCIUtils.addAllAttributes(attributes, entity.getKind());
+		Kind entityKind = entity.getKind();
+		if(entityKind != null) {
+			OCCIUtils.addAllAttributes(attributes, entityKind);
+		}
 		for(Mixin mixin : entity.getMixins()) {
 			OCCIUtils.addAllAttributes(attributes, mixin);
 		}	
@@ -44,8 +66,9 @@ public final class OCCIUtils
 	 */
 	public static void addAllAttributes(final Collection<Attribute> attributes, final Kind kind)
 	{
-		if (kind.getParent() != null) {
-			addAllAttributes(attributes, kind.getParent());
+		Kind kindParent = kind.getParent();
+		if (kindParent != null) {
+			addAllAttributes(attributes, kindParent);
 		}
 		attributes.addAll(kind.getAttributes());
 	}
@@ -64,13 +87,37 @@ public final class OCCIUtils
 		attributes.addAll(mixin.getAttributes());
 	}
 
+
 	/**
-	 * Compute the EMF feature name of an OCCI attribute name.
-	 * @param attributeName the given OCCI attribute name.
-	 * @return the computed EMF attribute name.
+	 * Get a kind from an extension. 
+	 * @param extension the given extension where kind will be searched.
+	 * @param kindTerm the term of the kind to search.
+	 * @return the found kind, otherwise null.
 	 */
-	public static String computeEmfAttributeNameFromOcciAttributeName(final String attributeName) {
-		int index = attributeName.lastIndexOf('.');
-		return (index != -1) ? attributeName.substring(index+1) : attributeName;
+	public static Kind getKindByTerm(final Extension extension, final String kindTerm) {
+		// Iterate over all kinds of the given extension.
+		for (Kind kind : extension.getKinds()) {
+			// If this kind has the same term that those searched then return this kind.
+			if (kind.getTerm().equals(kindTerm)) {
+				return kind;
+			}
+		}
+		// Kind not found.
+		return null;
+	}
+
+	/**
+	 * Get the configuration containing a given entity.
+	 * @param entity the given entity.
+	 * @return the entity's configuration.
+	 */
+	public static Configuration getConfiguration(final Entity entity) {
+		if(entity instanceof Resource) {
+			return (Configuration)entity.eContainer();
+		} else if(entity instanceof Link) {
+			return (Configuration)entity.eContainer().eContainer();
+		} else {
+			throw new Error("Given entity is not a resource or link");
+		}
 	}
 }
