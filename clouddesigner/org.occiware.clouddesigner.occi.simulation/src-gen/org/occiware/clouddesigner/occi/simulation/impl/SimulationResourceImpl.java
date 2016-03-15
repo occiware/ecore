2fc71d0bf283e5a2683218db534a8b6f7034fe03
mehdi <mehdi.ahmed-nacer@loria.fr>
2016-03-15 11:35:01 +0100

    update dependencies

---
+++ clouddesigner/org.occiware.clouddesigner.occi.simulation/src-gen/org/occiware/clouddesigner/occi/simulation/impl/SimulationResourceImpl.java
@@ -0,0 +1,39 @@
+/**
+ */
+package org.occiware.clouddesigner.occi.simulation.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.occiware.clouddesigner.occi.impl.ResourceImpl;
+
+import org.occiware.clouddesigner.occi.simulation.SimulationPackage;
+import org.occiware.clouddesigner.occi.simulation.SimulationResource;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Resource</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class SimulationResourceImpl extends ResourceImpl implements SimulationResource {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected SimulationResourceImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return SimulationPackage.Literals.SIMULATION_RESOURCE;
+	}
+
+} //SimulationResourceImpl
