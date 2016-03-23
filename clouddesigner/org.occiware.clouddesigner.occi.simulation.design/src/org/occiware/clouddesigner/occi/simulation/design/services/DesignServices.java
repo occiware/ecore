5b401217a8ec13f9f343353726347ce3cc7b6dd7
mehdi <mehdi.ahmed-nacer@loria.fr>
2016-03-23 11:40:55 +0100

    tag resource

--- clouddesigner/org.occiware.clouddesigner.occi.simulation.design/src/org/occiware/clouddesigner/occi/simulation/design/services/DesignServices.java
+++ clouddesigner/org.occiware.clouddesigner.occi.simulation.design/src/org/occiware/clouddesigner/occi/simulation/design/services/DesignServices.java
@@ -1,70 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Inria
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * - Philippe Merle <philippe.merle@inria.fr>
+ *******************************************************************************/
 package org.occiware.clouddesigner.occi.simulation.design.services;
 
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EcorePackage;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.sirius.business.api.session.Session;
 import org.eclipse.sirius.business.api.session.SessionManager;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-import org.occiware.clouddesigner.occi.Configuration;
+import org.occiware.clouddesigner.occi.Attribute;
 import org.occiware.clouddesigner.occi.Extension;
+import org.occiware.clouddesigner.occi.Kind;
+import org.occiware.clouddesigner.occi.Mixin;
+import org.occiware.clouddesigner.occi.OCCIFactory;
 import org.occiware.clouddesigner.occi.OCCIRegistry;
-import org.occiware.clouddesigner.occi.util.OCCIResourceSet;
+import org.occiware.clouddesigner.occi.Resource;
+
 
 public class DesignServices {
 
+	String scheme = "http://occiware.org/simulation#";
 
-	public Boolean isEDataType(EObject eObj) {
-		return eObj.eClass() == EcorePackage.eINSTANCE.getEDataType();
-	}
-
-
-	public void importExtension(Extension extension) {
-		extension = loadExtension("../org.occiware.clouddesigner.occi.simulation/model/Simulation.occie");
-		
-		Shell shell = Display.getCurrent().getActiveShell();
-		Session session = SessionManager.INSTANCE.getSession(extension);
-		String reg = OCCIRegistry.getInstance().getExtensionURI(extension.getScheme());
-		System.out.println(reg);
-		
-		//LoadExtensionDialog dialog = new LoadExtensionDialog(shell, session.getTransactionalEditingDomain());
-		//dialog.open();
-		URI uri = URI.createURI(reg);
-		session.addSemanticResource(uri, new NullProgressMonitor());
-		Resource resource = session.getTransactionalEditingDomain().getResourceSet().getResource(uri, true);
-		if (!resource.getContents().isEmpty() && (resource.getContents().get(0) instanceof Extension)
-				&& !extension.getImport().contains(resource.getContents().get(0))) {
-			extension.getImport().add((Extension) resource.getContents().get(0));
-		}
+	
+	public void tagDatacenter(Resource resource) {
+		tagResource(resource, "datacenter");
 	}
 	
-	/**
-	 * Load an OCCI extension.
-	 * @param extensionURI URI of the extension to load.
-	 * @return the loaded extension.
-	 */
-	public static Extension loadExtension(String extensionURI)
-	{
-		 return (Extension)loadOCCI(extensionURI);
+	public void tagHost(Resource resource) {
+		tagResource(resource, "host");
 	}
+	
+	public void tagVM(Resource resource) {
+		tagResource(resource, "VM");
+	}
+	
+	public void tagApplication(Resource resource) {
+		tagResource(resource, "cloudlet");
+	}
+	
+	public void tagResource(Resource resource, String term) {
+		String extensionURI = OCCIRegistry.getInstance().getExtensionURI(scheme);
+		Session session = SessionManager.INSTANCE.getSession(resource);
+		final org.eclipse.emf.ecore.resource.Resource extensionResource = resource.eResource().getResourceSet()
+				.getResource(URI.createURI(extensionURI, true), true);
+		final Extension extension = (Extension) extensionResource.getContents().get(0);
+		//ResourceSet resourceSet = session.getSessionResource().getResourceSet();
 
-	/**
-	 * Load an OCCI object.
-	 * @param uri URI of the OCCI object to load.
-	 * @return the loaded OCCI object.
-	 */
-	private static Object loadOCCI(String uri)
-	{
-		 // Create a new resource set.
-		 ResourceSet resourceSet = new OCCIResourceSet();
-		 // Load the OCCI resource.
-		 org.eclipse.emf.ecore.resource.Resource resource = resourceSet.getResource(URI.createURI(uri), true);
-		 // Return the first element.
-		 return resource.getContents().get(0);
+		if (!extensionResource.getContents().isEmpty() && (extensionResource.getContents().get(0) instanceof Extension)
+				&& !extension.getImport().contains(extensionResource.getContents().get(0))) {
+			extension.getImport().add((Extension) extensionResource.getContents().get(0));
+		}
+		for (Mixin mixin : extension.getMixins()) {
+			if(mixin.getTerm().contains(term)){
+				resource.getMixins().add(mixin);
+			}
+		}
 	}
 
 }
