60ab66a433a4f2ce7676b0c8a046ef12a09aca7e
philippemerle <philippe.merle@inria.fr>
2016-04-12 18:02:55 +0200

    Regenerate Java code from metamodel.

--- clouddesigner/org.occiware.clouddesigner.occi.docker.tests/src/org/occiware/clouddesigner/occi/docker/tests/DockerExample.java
+++ clouddesigner/org.occiware.clouddesigner.occi.docker.tests/src/org/occiware/clouddesigner/occi/docker/tests/DockerExample.java
@@ -1,12 +1,12 @@
 /**
- * Copyright (c) 2015-2016 Obeo, Inria
+ * Copyright (c) 2015-2016 Inria
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 	
  * Contributors:
- * - William Piers <william.piers@obeo.fr>
+ * -Fawaz Paraiso <fawaz.paraiso@inria.fr>
  * - Philippe Merle <philippe.merle@inria.fr>
  */
 package org.occiware.clouddesigner.occi.docker.tests;
