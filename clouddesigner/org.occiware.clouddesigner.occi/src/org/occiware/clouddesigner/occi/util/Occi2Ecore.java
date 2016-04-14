c8c7563dbd7aa7b9fa5304cdc684d00ca67f4f5f
philippemerle <philippe.merle@inria.fr>
2016-04-14 12:50:58 +0200

    Move OCCI to Ecore conversion functions to this class.

--- clouddesigner/org.occiware.clouddesigner.occi/src/org/occiware/clouddesigner/occi/util/Occi2Ecore.java
+++ clouddesigner/org.occiware.clouddesigner.occi/src/org/occiware/clouddesigner/occi/util/Occi2Ecore.java
@@ -46,4 +46,34 @@ public class Occi2Ecore
 	public static String convertEcoreNamespace2OcciScheme(final String ecoreNamespace) {
 		return ecoreNamespace.substring(0, ecoreNamespace.length()-ECORE_SUFFIX.length()) + '#';
 	}
+
+	/**
+	 * Converts an OCCI category term to an Ecore class name.
+	 * @param term the OCCI category term.
+	 * @return the Ecore class name.
+	 */
+	public static String convertOcciCategoryTerm2EcoreClassName(String term) {
+		return term.substring(0,1).toUpperCase() + term.substring(1);
+	}
+
+	/**
+	 * Convert an Ecore class name to an OCCI category term.
+	 * @param eclassName the given Ecore class name.
+	 * @return an OCCI category term.
+	 */
+	public static String convertEcoreClassName2OcciCategoryTerm(String eclassName)
+	{
+		return eclassName.substring(0, 1).toLowerCase() + eclassName.substring(1);
+	}
+
+	/**
+	 * Convert an OCCI attribute name to an Ecore feature name.
+	 * @param attributeName the given OCCI attribute name.
+	 * @return the converted Ecore attribute name.
+	 */
+	public static String convertOcciAttributeName2EcoreAttributeName(final String attributeName)
+	{
+		int index = attributeName.lastIndexOf('.');
+		return (index != -1) ? attributeName.substring(index+1) : attributeName;
+	}
 }
