cbfa62232adac41d2780f4439a203836aa94a246
philippemerle <philippe.merle@inria.fr>
2016-04-20 04:57:07 +0200

    Fix the generation of Sirius designers: * use new http://schemas.ogf.org/occi/core/ecore namespace. * add used metamodels. * don't prefix the name of a class by the name of its package. * remove call to service addKind as this is useless. * generate a popup menu to execute actions directly from the designer. * monitor generation of files. * generate NewConfigurationWizard automatically.

--- clouddesigner/org.occiware.clouddesigner.occi.emfgen.ui/src/org/occiware/clouddesigner/occi/emfgen/ui/popup/actions/GenUtils.java
+++ clouddesigner/org.occiware.clouddesigner.occi.emfgen.ui/src/org/occiware/clouddesigner/occi/emfgen/ui/popup/actions/GenUtils.java
@@ -19,6 +19,7 @@ import java.util.Collection;
 import java.util.Collections;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
@@ -41,18 +42,18 @@ import org.occiware.clouddesigner.occi.emfgen.ConverterUtils;
 
 public class GenUtils {
 
-	public static IProject genDesignProject(String projectName, String modelName, ProgressMonitorDialog dialog)
+	public static IProject genDesignProject(String projectName, String modelFileName, ProgressMonitorDialog dialog)
 			throws CoreException, IOException {
 		final IPath projectLocationPath = ResourcesPlugin.getWorkspace().getRoot().getLocation();
 		IProject project = ViewpointSpecificationProject.createNewViewpointSpecificationProject(
-				PlatformUI.getWorkbench(), projectName, projectLocationPath, modelName,
+				PlatformUI.getWorkbench(), projectName, projectLocationPath, modelFileName,
 				ViewpointSpecificationProject.INITIAL_OBJECT_NAME, ViewpointSpecificationProject.ENCODING_DEFAULT,
 				dialog);
 
 		// add dependency to the metamodel
-		IFile file = PDEProject.getManifest(project);
-		StringBuffer buffer = new StringBuffer();
-		BufferedReader in = new BufferedReader(new InputStreamReader(file.getContents()));
+		IFile manifest = PDEProject.getManifest(project);
+		StringBuilder buffer = new StringBuilder();
+		BufferedReader in = new BufferedReader(new InputStreamReader(manifest.getContents()));
 		String inputLine;
 		while ((inputLine = in.readLine()) != null) {
 			buffer.append(inputLine + "\n");
@@ -62,7 +63,69 @@ public class GenUtils {
 			}
 		}
 		in.close();
-		file.setContents(new ByteArrayInputStream(buffer.toString().getBytes()), 0, null);
+		manifest.setContents(new ByteArrayInputStream(buffer.toString().getBytes()), 0, dialog.getProgressMonitor());
+
+		String modelName = modelFileName.substring(0, modelFileName.indexOf('.'));
+
+		// add extension to plugin.xml
+		IFile pluginXml = PDEProject.getPluginXml(project);
+		buffer = new StringBuilder();
+		in = new BufferedReader(new InputStreamReader(pluginXml.getContents()));
+		while ((inputLine = in.readLine()) != null) {
+			if (inputLine.startsWith("</plugin>")) {
+				buffer.append("  <extension point=\"org.eclipse.ui.newWizards\">\n");
+				buffer.append("    <category\n");
+				buffer.append("        id=\"org.occiware.clouddesigner\"\n");
+				buffer.append("        name=\"Cloud Designer\">\n");
+				buffer.append("    </category>\n");
+				buffer.append("    <wizard\n");
+				buffer.append("        category=\"org.occiware.clouddesigner\"\n");
+				buffer.append("        class=\"").append(projectName).append(".wizard.NewConfigurationWizard\"\n");
+				buffer.append("        icon=\"images/Configuration.gif\"\n");
+				buffer.append("        id=\"").append(projectName).append(".wizard.NewConfigurationWizard\"\n");
+				buffer.append("        name=\"").append(modelName).append(" Configuration File\">\n");
+				buffer.append("    </wizard>\n");
+				buffer.append("  </extension>\n");
+			}
+			buffer.append(inputLine + "\n");
+		}
+		in.close();
+		pluginXml.setContents(new ByteArrayInputStream(buffer.toString().getBytes()), 0, dialog.getProgressMonitor());		
+
+		buffer = new StringBuilder();
+		buffer.append("/*******************************************************************************\n");
+		buffer.append(" * Copyright (c) 2015-2016 Obeo, Inria\n");
+		buffer.append(" * All rights reserved. This program and the accompanying materials\n");
+		buffer.append(" * are made available under the terms of the Eclipse Public License v1.0\n");
+		buffer.append(" * which accompanies this distribution, and is available at\n");
+		buffer.append(" * http://www.eclipse.org/legal/epl-v10.html\n");
+		buffer.append(" *\n");
+		buffer.append(" * Contributors:\n");
+		buffer.append(" * - William Piers <william.piers@obeo.fr>\n");
+		buffer.append(" * - Philippe Merle <philippe.merle@inria.fr>\n");
+		buffer.append(" *******************************************************************************/\n");
+		buffer.append("\n");
+		buffer.append("package ").append(projectName).append(".wizard;\n");
+		buffer.append("\n");
+		buffer.append("import org.occiware.clouddesigner.occi.design.utils.NewDiagramWizard;\n");
+		buffer.append("\n");
+		buffer.append("/**\n");
+		buffer.append(" * This is a simple wizard for creating a new ").append(modelName).append(" model file.\n");
+		buffer.append(" */\n");
+		buffer.append("\n");
+
+		buffer.append("public class NewConfigurationWizard extends NewDiagramWizard\n");
+		buffer.append("{\n");
+		buffer.append("\tpublic NewConfigurationWizard()\n");
+		buffer.append("\t{\n");
+		String tmp = modelName.substring(0,1).toUpperCase() + modelName.substring(1);
+		buffer.append("\t\tsuper(\"viewpoint:/").append(projectName).append("/").append(tmp).append("Configuration\", \"").append(modelName).append("\", \"").append(tmp).append("Configuration Diagram\");\n");
+		buffer.append("\t}\n");
+		buffer.append("}\n");
+		IFolder folder = project.getFolder("src/" + projectName.replaceAll("\\.",  "/") + "/wizard");
+		folder.create(false, false, dialog.getProgressMonitor());
+		IFile newConfigurationWizard = folder.getFile("NewConfigurationWizard.java");
+		newConfigurationWizard.create(new ByteArrayInputStream(buffer.toString().getBytes()), 0, dialog.getProgressMonitor());		
 		return project;
 	}
 
