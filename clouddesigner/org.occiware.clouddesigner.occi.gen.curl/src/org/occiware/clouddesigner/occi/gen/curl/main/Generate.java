6c98148efbd312b7894222299a3d46dd419271be
Philippe Merle <Philippe.merle@inria.fr>
2016-06-02 11:19:27 +0200

    Update header.

--- clouddesigner/org.occiware.clouddesigner.occi.gen.curl/src/org/occiware/clouddesigner/occi/gen/curl/main/Generate.java
+++ clouddesigner/org.occiware.clouddesigner.occi.gen.curl/src/org/occiware/clouddesigner/occi/gen/curl/main/Generate.java
@@ -1,13 +1,14 @@
-/*******************************************************************************
- * Copyright (c) 2008, 2012 Obeo.
+/**
+ * Copyright (c) 2015-2016 Inria
+ *  
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *     Obeo - initial API and implementation
- *******************************************************************************/
+ * - Philippe Merle <philippe.merle@inria.fr>
+ */
 package org.occiware.clouddesigner.occi.gen.curl.main;
 
 import java.io.File;
