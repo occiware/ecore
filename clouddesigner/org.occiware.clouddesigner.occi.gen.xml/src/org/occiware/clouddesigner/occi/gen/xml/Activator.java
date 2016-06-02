5deace63491c0f7f5f33052d2637f7999ec86db2
Philippe Merle <Philippe.merle@inria.fr>
2016-06-02 11:09:43 +0200

    Update header.

--- clouddesigner/org.occiware.clouddesigner.occi.gen.xml/src/org/occiware/clouddesigner/occi/gen/xml/Activator.java
+++ clouddesigner/org.occiware.clouddesigner.occi.gen.xml/src/org/occiware/clouddesigner/occi/gen/xml/Activator.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
- * Copyright (c) 2015 Inria
+ * Copyright (c) 2015-2016 Inria
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +8,7 @@
  * 
  * Contributors:
  *     Jean Parpaillon <jean.parpaillon@inria.fr>
+ *     Philippe Merle <philippe.merle@inria.fr>
  *******************************************************************************/
 package org.occiware.clouddesigner.occi.gen.xml;
 
